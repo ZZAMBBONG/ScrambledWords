9e6da38e05ffb74a3b83661059f310146a1ffc62
ZZAMBBONG <cihmin2@gmail.com>
2025-03-19 18:14:20 +0900

    Initial Commit

---
+++ ScrambledWords/ScrambledWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambledWordsApp.swift
+//  ScrambledWords
+//
+//  Created by Hun Min Cho on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrambledWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
