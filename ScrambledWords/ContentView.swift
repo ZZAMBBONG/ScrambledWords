429c236d884b1c3006f3a69479d87cc216c2e611
ZZAMBBONG <cihmin2@gmail.com>
2025-03-19 19:29:36 +0900

    오렌지게임

--- ScrambledWords/ContentView.swift
+++ ScrambledWords/ContentView.swift
@@ -7,18 +7,67 @@
 
 import SwiftUI
 
+//Hashable
+
 struct ContentView: View {
+    
+    @State var letters: [String] = ["O","R", "A", "N", "G", "E"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { proxy in
+            ZStack{
+                Color.background
+                    .ignoresSafeArea()
+                VStack {
+                    VStack {
+                        Spacer()
+                        Image("orange")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                        Spacer()
+                        HStack{
+                            VStack{
+                                LetterView(character: "")
+                                Rectangle()
+                                    .fill(Color.white)
+                                    .frame(width: 25, height: 2)
+                            }
+                        }
+                        .padding(.bottom, 20)
+                    }
+                    .frame(width: proxy.size.width * 0.9,
+                           height: proxy.size.width * 0.9)
+                    .overlay{
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.border, lineWidth: 2)
+                    }
+                    Text("Score 0")
+                        .font(.system(size: 15))
+                        .foregroundStyle(Color.white)
+                        .padding(.top)
+                    HStack{
+                        ForEach(letters, id: \.self){ letter in
+                            LetterView(character: letter)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct LetterView: View {
+    let character: String
+    var body: some View {
+        Text(character)
+            .font(.system(size: 15, weight: .semibold))
+            .foregroundStyle(Color.white)
+            .frame(width: 30, height: 30)
+            .background(Color.white.opacity(0.4))
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
 }
